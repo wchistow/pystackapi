ff2ea3d55af5329eb0e50a446fafa204a3b0b0a6
Vladimir Chistov <wchistow@yandex.ru>
2023-06-01 10:17:35 +0300

    add `call` method to client

--- src/pystackapi/client.py
+++ src/pystackapi/client.py
@@ -1,10 +1,23 @@
 import requests
 
+from .errors import HttpError
+
 
 class Client:
     version = '2.3'
-    base_url = f'api.stackexchange.org/{version}'
+    base_url = f'https://api.stackexchange.com/{version}/'
 
-    def __init__(self, site: str, api_key: str | None = None):
+    def __init__(self, site: str, api_key: str | None = None) -> None:
         self.site = site
         self.api_key = api_key
+
+    def call(self, query: str) -> dict:
+        params = f'?site={self.site}'
+        if self.api_key is not None:
+            params += f'&access_token={self.api_key}'
+
+        response = requests.get(f'{self.base_url}{query}{params}')
+
+        if response.status_code != 200:
+            raise HttpError(response.status_code)
+        return response.json()

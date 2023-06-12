7204a0a9b505a24955b50309ae18d274bff8e172
Vladimir Chistov <wchistow@yandex.ru>
2023-06-12 11:43:54 +0300

    add method `get_comments` to model `User`

--- src/pystackapi/models/user.py
+++ src/pystackapi/models/user.py
@@ -3,6 +3,7 @@ from typing import Any
 from .base import BaseModel, SiteT
 from .answer import Answer
 from .badge import Badge
+from .comment import Comment
 
 
 class User(BaseModel):
@@ -16,3 +17,11 @@ class User(BaseModel):
     def get_badges(self, **kwargs: Any) -> list[Badge]:
         response = self._client.call(f'users/{self.account_id}/badges', **kwargs)
         return [Badge(self._client, dict(data)) for data in response]
+
+    def get_comments(self, toid: int | None = None, **kwargs: Any) -> list[Comment]:
+        """Implements API methods `users/{ids}/comments` and `users/{ids}/comments/{toid}`."""
+        if toid is not None:
+            response = self._client.call(f'users/{self.account_id}/comments/{toid}', **kwargs)
+        else:
+            response = self._client.call(f'users/{self.account_id}/comments', **kwargs)
+        return [Comment(self._client, dict(data)) for data in response]

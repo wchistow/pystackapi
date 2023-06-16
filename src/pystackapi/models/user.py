9977b805b802c16cc77c910a597f6394e2e30d18
Vladimir Chistov <wchistow@yandex.ru>
2023-06-16 11:05:35 +0300

    add method `_get_call` to class `models.User`

--- src/pystackapi/models/user.py
+++ src/pystackapi/models/user.py
@@ -1,32 +1,40 @@
-from typing import Any
+from typing import Any, Type, TypeVar
 
 from .base import BaseModel, SiteT
 from .answer import Answer
 from .badge import Badge
 from .comment import Comment
 from .priviledge import Privilege
+from .question import Question
+
+
+BaseModelT_co = TypeVar('BaseModelT_co', bound=BaseModel, covariant=True)
 
 
 class User(BaseModel):
     def __init__(self, client: SiteT, data: dict[Any, Any]) -> None:
         super().__init__(client, data)
 
+    def _get_call(self, url: str, model: Type[BaseModelT_co], **kwargs: Any) -> list[BaseModelT_co]:
+        response = self._client.call(url, **kwargs)
+        return [model(self._client, dict(data)) for data in response]
+
     def get_answers(self, **kwargs: Any) -> list[Answer]:
-        response = self._client.call(f'users/{self.account_id}/answers', **kwargs)
-        return [Answer(self._client, dict(data)) for data in response]
+        return self._get_call(f'users/{self.account_id}/answers', Answer, **kwargs)
 
     def get_badges(self, **kwargs: Any) -> list[Badge]:
-        response = self._client.call(f'users/{self.account_id}/badges', **kwargs)
-        return [Badge(self._client, dict(data)) for data in response]
+        return self._get_call(f'users/{self.account_id}/badges', Badge, **kwargs)
 
     def get_comments(self, toid: int | None = None, **kwargs: Any) -> list[Comment]:
         """Implements API methods `users/{ids}/comments` and `users/{ids}/comments/{toid}`."""
         if toid is not None:
-            response = self._client.call(f'users/{self.account_id}/comments/{toid}', **kwargs)
+            url = f'users/{self.account_id}/comments/{toid}'
         else:
-            response = self._client.call(f'users/{self.account_id}/comments', **kwargs)
-        return [Comment(self._client, dict(data)) for data in response]
+            url = f'users/{self.account_id}/comments'
+        return self._get_call(url, Comment, **kwargs)
 
-    def get_privileges(self, **kwargs) -> list[Privilege]:
-        response = self._client.call(f'users/{self.account_id}/privileges', **kwargs)
-        return [Privilege(self._client, dict(data)) for data in response]
+    def get_privileges(self, **kwargs: Any) -> list[Privilege]:
+        return self._get_call(f'users/{self.account_id}/privileges', Privilege, **kwargs)
+
+    def get_questions(self, **kwargs: Any) -> list[Question]:
+        return self._get_call(f'users/{self.account_id}/questions', Question, **kwargs)

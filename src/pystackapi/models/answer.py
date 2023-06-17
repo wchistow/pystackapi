d32b827a1d2207ba5085fa4c6f2e2f5fdb87697a
Vladimir Chistov <wchistow@yandex.ru>
2023-06-17 10:54:28 +0300

    add method `get_comments` to model `Answer`

--- src/pystackapi/models/answer.py
+++ src/pystackapi/models/answer.py
@@ -1,8 +1,12 @@
 from typing import Any
 
 from .base import BaseModel, SiteT
+from .comment import Comment
 
 
 class Answer(BaseModel):
     def __init__(self, client: SiteT, data: dict[Any, Any]) -> None:
         super().__init__(client, data)
+
+    def get_comments(self, **kwargs: Any) -> list[Comment]:
+        return self._get_call(f'answers/{self.answer_id}/comments', Comment, **kwargs)

7f16553be20f97b4ab286ee11f748b5501437912
Vladimir Chistov <wchistow@yandex.ru>
2023-06-23 09:55:20 +0300

    move all tests for models to folder `test_models`

--- tests/main.py
+++ tests/main.py
@@ -4,10 +4,10 @@ import sys
 
 sys.path.insert(0, '../src')
 
-import test_answer
-import test_base
-import test_question
-import test_user
+import test_models.test_answer
+import test_models.test_base
+import test_models.test_question
+import test_models.test_user
 
 if __name__ == '__main__':
     lest.run()

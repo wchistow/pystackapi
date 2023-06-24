dd9dbafe4e449b0b805b73d0e41edbdac08b126e
Vladimir Chistov <wchistow@yandex.ru>
2023-06-24 11:46:12 +0300

    add file `__init__.py` to package `test_models`

--- tests/main.py
+++ tests/main.py
@@ -4,10 +4,7 @@ import sys
 
 sys.path.insert(0, '../src')
 
-import test_models.test_answer
-import test_models.test_base
-import test_models.test_question
-import test_models.test_user
+from test_models import *
 
 import test_response
 import test_item
---
+++ tests/test_models/__init__.py
@@ -0,0 +1,4 @@
+from . import test_answer
+from . import test_base
+from . import test_question
+from . import test_user

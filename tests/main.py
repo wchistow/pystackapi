e00d4b11de229ed4e97ad975f97881921da63992
Vladimir Chistov <wchistow@yandex.ru>
2023-06-24 11:43:30 +0300

    add tests for class `Item`

--- tests/main.py
+++ tests/main.py
@@ -8,7 +8,9 @@ import test_models.test_answer
 import test_models.test_base
 import test_models.test_question
 import test_models.test_user
+
 import test_response
+import test_item
 
 if __name__ == '__main__':
     lest.run()

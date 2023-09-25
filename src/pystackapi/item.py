ffab94fa1c01b400700f9336e6a4f1ed7372e050
antoninoLorenzo <collab.beatbuster@gmail.com>
2023-09-25 09:43:50 +0200

    Added Item __str__

--- src/pystackapi/item.py
+++ src/pystackapi/item.py
@@ -6,3 +6,11 @@ class Item(UserDict[str, Any]):
     """Useful class to discover JSON data."""
     def __getattr__(self, name: str) -> Any:
         return self.data[name]
+
+    """Improved Readability for Item Data"""
+    def __str__(self):
+        out = []
+        for attr in self.data:
+            out.append(f'{attr}: {self.data[attr]}\n')
+        
+        return ''.join(out)
